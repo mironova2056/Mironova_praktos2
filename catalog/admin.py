32a10936dfaf4cde04565927af49b3e4e1cd9a33
mironova2056 <145144059+mironova2056@users.noreply.github.com>
2024-11-12 17:25:21 +0700

    add applicationadmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -32,7 +32,35 @@ class ApplicationAdminForm(forms.ModelForm):
         if status == 'done' and not cleaned_data.get('design_image'):
             raise ValidationError('Пожалуйста, загрузите изображение')
         return cleaned_data
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    form = ApplicationAdminForm
+    list_display = ('title', 'user', 'status', 'created_at', 'category')
+    readonly_fields = ('user', 'title', 'description', 'category', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'user__username', 'description')
+    exclude = ['image']
 
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if obj:
+            status = obj.status
+
+            if status == 'active':
+                if 'design_image' in form.base_fields:
+                    form.base_fields['design_image'].required = False
+                    del form.base_fields['design_image']
+            if status == 'done':
+                if 'comment' in form.base_fields:
+                    form.base_fields['comment'].required = False
+                    del form.base_fields['comment']
+        return form
+    def save_model(self, request, obj, form, change):
+        if form.is_valid():
+            obj.comment = form.cleaned_data.get('comment', '')
+            obj.design_image = form.cleaned_data.get('design_image', None)
+            with transaction.atomic():
+                obj.save()
 
 admin.site.register(Category)
 
